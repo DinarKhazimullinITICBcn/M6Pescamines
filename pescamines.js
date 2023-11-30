ab823fd026c5ca89e838dbc19b16f5cfcea2ee5b
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-30 22:44:06 +0100

    Modificat la part de css del pescamines

--- pescamines.js
+++ pescamines.js
@@ -1,6 +1,9 @@
 let files = 0;
 let columnes = 0;
 let casellesFaltants = 0;
+let mines = 0;
+let temps = 0;
+let intervalId;
 function iniciarPartida() {
     files = parseInt(prompt("Insereix files"));
     columnes = parseInt(prompt("Insereix columnes"));
@@ -8,16 +11,28 @@ function iniciarPartida() {
     columnes = Math.min(Math.max(columnes, 10), 30);
     files = isNaN(files) ? 10 : Math.min(Math.max(parseInt(files), 10), 30);
     columnes = isNaN(columnes) ? 10 : Math.min(Math.max(parseInt(columnes), 10), 30);
-
+    clearInterval(intervalId);
+    temps = 0;
+    var tempsString = String(temps).padStart(3, '0');
+    document.getElementById('temps').textContent = tempsString;
+    intervalId = setInterval(function() {
+    temps++;
+    var tempsString = String(temps).padStart(3, '0');
+    document.getElementById('temps').textContent = tempsString;
+    if (temps >= 999) {
+        clearInterval(intervalId);
+    }
+    }, 1000);
     creaTaulell();
     setMines();
+    let contadorMines = document.getElementById('mines');
+    contadorMines.textContent = mines;
     calculaAdjacents();
-    console.log(casellesFaltants);
 }
 function creaTaulell() {
     let div = document.getElementById('taulell');
     let taula = "";
-    taula = "<table>";
+    taula = "<table id='buscamines'>";
     for (let fila = 0; fila < files; fila++) {
         taula += "<tr>";
         for (let columna = 0; columna < columnes; columna++) {
@@ -29,7 +44,6 @@ function creaTaulell() {
     div.innerHTML = taula;
 }
 function obreCasella(fila, columna) {
-    console.log(casellesFaltants);
     let casella = document.getElementById(`${fila}-${columna}`);
     if (esMina(fila, columna)) {
         acabarPartida();
@@ -39,7 +53,7 @@ function obreCasella(fila, columna) {
     } else {
         let adjacent = Number(casella.dataset.numMines);
         obreCaselles(fila, columna, adjacent);
-        if (casellesFaltants === 0) {
+        if (casellesFaltants < 0) {
             acabarPartida();
             setTimeout(function() {
                 alert('Has ganat');
@@ -56,7 +70,7 @@ function esMina(fila, columna) {
     return false;
 }
 function setMines() {
-    let mines = (Math.round((files * columnes) * 0.17));
+    mines = (Math.round((files * columnes) * 0.17));
     casellesFaltants = Math.round(files * columnes) - mines;
     for (let i = 0; i < mines; i++) {
         let fila = Math.floor(Math.random() * files);
@@ -77,8 +91,12 @@ function acabarPartida() {
             if (casella.dataset.mina === "true") {
                 casella.style.backgroundImage = "url('img/mina20px.jpg')";
             }
+            if (!casella.onclick) {
+                casella.style.opacity = '0.90';
+            }
         }
     }
+    clearInterval(intervalId);
 }
 function calculaAdjacents() {
     const posicioMina = [
@@ -115,12 +133,13 @@ function obreCaselles(fila, columna, numAdjacent) {
     casellesFaltants--;
     let casella = document.getElementById(`${fila}-${columna}`);
     casella.style.backgroundImage = "";
-    casella.textContent = casella.dataset.numMines;
+    casella.textContent = casella.dataset.numMines != 0 ? casella.dataset.numMines : '';
     casella.dataset.numMines = numAdjacent.toString();
     casella.dataset.obert = "true";
     if (numAdjacent === 0) {
         obreCasellaAdjacent(fila, columna);
     }
+    colorLletra(fila, columna);
 }
 
 function obreCasellaAdjacent(fila, columna) {
@@ -136,13 +155,43 @@ function obreCasellaAdjacent(fila, columna) {
             let casellaAdjacent = document.getElementById(`${filaAdjacent}-${columnaAdjacent}`);
             if (casellaAdjacent.dataset.mina !== "true" && casellaAdjacent.dataset.obert !== "true") {
                 casellaAdjacent.style.backgroundImage = "";
-                casellaAdjacent.textContent = casellaAdjacent.dataset.numMines;
+                casellaAdjacent.textContent = casellaAdjacent.dataset.numMines != 0 ? casellaAdjacent.dataset.numMines : '';
                 casellaAdjacent.dataset.obert = "true";
                 casellesFaltants--;
                 if (casellaAdjacent.dataset.numMines === "0") {
                     obreCasellaAdjacent(filaAdjacent, columnaAdjacent);
                 }
+                colorLletra(filaAdjacent, columnaAdjacent);
             }
         }
+    }
+}
+function colorLletra(fila, columna) {
+    let casella = document.getElementById(`${fila}-${columna}`);
+    switch (casella.textContent) {
+        case '1':
+            casella.style.color = 'blue';
+            break;
+        case '2':
+            casella.style.color = 'green';
+            break;
+        case '3':
+            casella.style.color = 'red';
+            break;
+        case '4':
+            casella.style.color = 'purple';
+            break;
+        case '5':
+            casella.style.color = 'yellow';
+            break;
+        case '6':
+            casella.style.color = 'teal';
+            break;
+        case '7':
+            casella.style.color = 'darkgrey';
+            break;
+        case '8':
+            casella.style.color = 'lightgrey';
+            break;
     }
 }
